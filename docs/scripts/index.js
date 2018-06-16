76f3f1644f9a7403560a3c753f2a33ed3df40dce
Ryszard <ryszard_rowinski@hotmail.co.uk>
2018-06-16 17:15:45 +0100

    Working API read and general page maintenance
    - Stopped using Auth for now
    - Made API Key and Client ID inputs for now so that they are not public.
    - Removed PHP page, working fully client side.
    - Can read Ratings data and display in a table
    - Moved JS into it's on file
    - Added basic page layout

---
+++ docs/scripts/index.js
@@ -0,0 +1,133 @@
+﻿// Global Variables
+var HEADER_ROW = 2;
+var RATINGS_STARTING_ROW = 3;
+
+
+// Get data from goole sheets
+function makeApiCall() {
+    var params = {
+        // The ID of the spreadsheet to retrieve data from.
+        spreadsheetId: '1ZwYgrpV0BrPkWbbnTdD1csZ2HoYGvWwkMY7po3YZXnI',  // TODO: Update placeholder value.
+
+        // The A1 notation of the values to retrieve.
+        range: 'BCB Reviews',  // TODO: Update placeholder value.
+
+        // How values should be represented in the output.
+        // The default render option is ValueRenderOption.FORMATTED_VALUE.
+        //valueRenderOption: '',  // TODO: Update placeholder value.
+
+        // How dates, times, and durations should be represented in the output.
+        // This is ignored if value_render_option is
+        // FORMATTED_VALUE.
+        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+        //dateTimeRenderOption: '',  // TODO: Update placeholder value.
+    };
+
+    var request = gapi.client.sheets.spreadsheets.values.get(params);
+    request.then(function (response) {
+        // TODO: Change code below to process the `response` object:
+        console.log(response.result);
+
+        createRatingTable(response.result);
+
+    }, function (reason) {
+        console.error('error: ' + reason.result.error.message);
+    });
+}
+
+function initClient() {
+    var API_KEY = $("#APIKEY").val();  // TODO: Update placeholder with desired API key.
+
+    var CLIENT_ID = $("#CLIENTID").val();  // TODO: Update placeholder with desired client ID.
+
+    // TODO: Authorize using one of the following scopes:
+    //   'https://www.googleapis.com/auth/drive'
+    //   'https://www.googleapis.com/auth/drive.file'
+    //   'https://www.googleapis.com/auth/drive.readonly'
+    //   'https://www.googleapis.com/auth/spreadsheets'
+    //   'https://www.googleapis.com/auth/spreadsheets.readonly'
+    var SCOPE = 'https://www.googleapis.com/auth/drive.readonly';
+
+    gapi.client.init({
+        'apiKey': API_KEY,
+        'clientId': CLIENT_ID,
+        'scope': SCOPE,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function () {
+        //gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+        //updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        makeApiCall();
+    })
+    ;
+}
+
+
+//Not using yet
+
+//function handleClientLoad() {
+//    gapi.load('client', initClient);
+//}
+
+//function updateSignInStatus(isSignedIn) {
+//  if (isSignedIn) {
+//    makeApiCall();
+//  }
+//}
+
+//function handleSignInClick(event) {
+//  gapi.auth2.getAuthInstance().signIn();
+//}
+
+//function handleSignOutClick(event) {
+//  gapi.auth2.getAuthInstance().signOut();
+//}
+
+// Use DATA from API to build up ratings table
+function createRatingTable(result) {
+    var tableData,
+        tableHeader,
+        tableBody;
+            
+    tableData = '<table width="320" border="1">';
+
+    // Table header
+    tableHeader = '<thead>'
+                    + '<tr>'
+                        + '<th>' + result.values[HEADER_ROW][0] + '</th>'
+                        + '<th>' + result.values[HEADER_ROW][result.values[HEADER_ROW].length - 1] + '</th>'
+                    + '</tr>'
+                + '</thead>';
+
+    // Table Body
+    tableBody = '<tbody>';
+    for (var row = RATINGS_STARTING_ROW; row < result.values.length; row++) {
+        tableBody = tableBody + '<tr>';
+        for (var col = 0; col < 2; col++) {
+            if (col == 0) { // Name
+                tableBody = tableBody + '<td>' + result.values[row][col] + '</td>';
+            }
+            else { // Rating (in the last row element)
+                tableBody = tableBody + '<td>' + result.values[row][result.values[row].length - 1];
+            }
+        }
+        tableBody = tableBody + '</tr>';
+    }
+
+    // Build up table and append
+    tableData = tableData + tableHeader + tableBody + '</table>';
+    $('#divRatings').append(tableData);
+}
+
+/*
+    Event bindings
+*/
+$(document).ready(function () {
+    $("#getReviewData").click(function () {
+        if ($("#APIKEY").val() != "" && $("#CLIENTID").val() != "") {
+            gapi.load('client', initClient);
+        }
+        else {
+            alert("Failed");
+        }
+    });
+});
